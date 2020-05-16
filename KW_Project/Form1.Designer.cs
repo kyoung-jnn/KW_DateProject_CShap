64d7ec44cd5fc23738b2c4b8fddf7e8723cc359a
Rohkyoungjin <rudwls468@gmail.com>
2020-05-16 21:08:35 +0900

    로그인 폼
    임시 구현

--- KW_Project/Form1.Designer.cs
+++ KW_Project/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KW_Project
 {
-    partial class Menu
+    partial class loginForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,20 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Menu
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::KW_Project.Properties.Resources.heart;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(308, 124);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(193, 25);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(308, 194);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(193, 25);
+            this.txtPwd.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F);
+            this.label1.Location = new System.Drawing.Point(308, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "비밀번호";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F);
+            this.label2.Location = new System.Drawing.Point(308, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "학번";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnLogin.Location = new System.Drawing.Point(313, 249);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(87, 40);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "로그인";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Magneto", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(119, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(342, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "KwangWoon Date Project";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRegister.Location = new System.Drawing.Point(408, 249);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(87, 40);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "회원가입";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // loginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(429, 377);
-            this.Name = "Menu";
-            this.Text = "MENU";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(213)))), ((int)(((byte)(213)))));
+            this.ClientSize = new System.Drawing.Size(552, 345);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "loginForm";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
 
