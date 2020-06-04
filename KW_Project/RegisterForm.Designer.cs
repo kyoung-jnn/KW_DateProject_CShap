35c6754cdaddca3f447ec6df1b4aa3457c7ff396
jiwonkim97 <jiwonkim97@naver.com>
2020-06-04 22:00:38 +0900

    200604_22:00 남/여 데이터베이스 구분

--- KW_Project/RegisterForm.Designer.cs
+++ KW_Project/RegisterForm.Designer.cs
@@ -38,6 +38,7 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
             this.R_txtPwd2 = new System.Windows.Forms.TextBox();
+            this.cmbSex = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,9 +46,10 @@
             // 
             this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnBack.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnBack.Location = new System.Drawing.Point(404, 279);
+            this.btnBack.Location = new System.Drawing.Point(354, 223);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(87, 40);
+            this.btnBack.Size = new System.Drawing.Size(76, 32);
             this.btnBack.TabIndex = 14;
             this.btnBack.Text = "뒤로가기";
             this.btnBack.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@
             // 
             this.btnRegisterConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnRegisterConfirm.Font = new System.Drawing.Font("휴먼둥근헤드라인", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnRegisterConfirm.Location = new System.Drawing.Point(311, 279);
+            this.btnRegisterConfirm.Location = new System.Drawing.Point(272, 223);
+            this.btnRegisterConfirm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRegisterConfirm.Name = "btnRegisterConfirm";
-            this.btnRegisterConfirm.Size = new System.Drawing.Size(87, 40);
+            this.btnRegisterConfirm.Size = new System.Drawing.Size(76, 32);
             this.btnRegisterConfirm.TabIndex = 13;
             this.btnRegisterConfirm.Text = "회원가입";
             this.btnRegisterConfirm.UseVisualStyleBackColor = true;
@@ -69,9 +72,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F);
-            this.label2.Location = new System.Drawing.Point(306, 81);
+            this.label2.Location = new System.Drawing.Point(268, 65);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 21);
+            this.label2.Size = new System.Drawing.Size(40, 17);
             this.label2.TabIndex = 12;
             this.label2.Text = "학번";
             // 
@@ -79,43 +82,46 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F);
-            this.label1.Location = new System.Drawing.Point(305, 146);
+            this.label1.Location = new System.Drawing.Point(267, 117);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.Size = new System.Drawing.Size(72, 17);
             this.label1.TabIndex = 11;
             this.label1.Text = "비밀번호";
             // 
             // R_txtPwd1
             // 
-            this.R_txtPwd1.Location = new System.Drawing.Point(307, 173);
+            this.R_txtPwd1.Location = new System.Drawing.Point(269, 138);
+            this.R_txtPwd1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.R_txtPwd1.Name = "R_txtPwd1";
             this.R_txtPwd1.PasswordChar = '*';
-            this.R_txtPwd1.Size = new System.Drawing.Size(193, 25);
+            this.R_txtPwd1.Size = new System.Drawing.Size(169, 21);
             this.R_txtPwd1.TabIndex = 10;
             // 
             // R_txtId
             // 
-            this.R_txtId.Location = new System.Drawing.Point(307, 107);
+            this.R_txtId.Location = new System.Drawing.Point(269, 86);
+            this.R_txtId.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.R_txtId.Name = "R_txtId";
-            this.R_txtId.Size = new System.Drawing.Size(193, 25);
+            this.R_txtId.Size = new System.Drawing.Size(169, 21);
             this.R_txtId.TabIndex = 9;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(196, 23);
+            this.label3.Location = new System.Drawing.Point(172, 18);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(158, 38);
+            this.label3.Size = new System.Drawing.Size(129, 31);
             this.label3.TabIndex = 15;
             this.label3.Text = "Register";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::KW_Project.Properties.Resources.heart;
-            this.pictureBox1.Location = new System.Drawing.Point(28, 79);
+            this.pictureBox1.Location = new System.Drawing.Point(24, 63);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(233, 240);
+            this.pictureBox1.Size = new System.Drawing.Size(204, 192);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
@@ -124,26 +130,41 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("휴먼둥근헤드라인", 12F);
-            this.label4.Location = new System.Drawing.Point(306, 214);
+            this.label4.Location = new System.Drawing.Point(268, 171);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(140, 21);
+            this.label4.Size = new System.Drawing.Size(112, 17);
             this.label4.TabIndex = 18;
             this.label4.Text = "비밀번호 확인";
             // 
             // R_txtPwd2
             // 
-            this.R_txtPwd2.Location = new System.Drawing.Point(307, 239);
+            this.R_txtPwd2.Location = new System.Drawing.Point(269, 191);
+            this.R_txtPwd2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.R_txtPwd2.Name = "R_txtPwd2";
             this.R_txtPwd2.PasswordChar = '*';
-            this.R_txtPwd2.Size = new System.Drawing.Size(193, 25);
+            this.R_txtPwd2.Size = new System.Drawing.Size(169, 21);
             this.R_txtPwd2.TabIndex = 17;
+            // 
+            // cmbSex
+            // 
+            this.cmbSex.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.cmbSex.FormattingEnabled = true;
+            this.cmbSex.Items.AddRange(new object[] {
+            "남자",
+            "여자"});
+            this.cmbSex.Location = new System.Drawing.Point(354, 29);
+            this.cmbSex.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbSex.Name = "cmbSex";
+            this.cmbSex.Size = new System.Drawing.Size(65, 20);
+            this.cmbSex.TabIndex = 20;
             // 
             // RegisterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(213)))), ((int)(((byte)(213)))));
-            this.ClientSize = new System.Drawing.Size(552, 345);
+            this.ClientSize = new System.Drawing.Size(483, 276);
+            this.Controls.Add(this.cmbSex);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.R_txtPwd2);
             this.Controls.Add(this.pictureBox1);
@@ -154,6 +175,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.R_txtPwd1);
             this.Controls.Add(this.R_txtId);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "RegisterForm";
             this.Text = "RegisterForm";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -174,5 +196,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox R_txtPwd2;
+        private System.Windows.Forms.ComboBox cmbSex;
     }
 }
